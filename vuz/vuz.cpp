58d45f27417088ab40b654413130499b1f6fbc31
burbon132 <burbon132@users.noreply.github.com>
2020-04-23 20:08:50 +0300

    Исходный вид

--- vuz/vuz.cpp
+++ vuz/vuz.cpp
@@ -3,18 +3,10 @@
 #include<string>
 using namespace std;
 int main() {
-	int a;
-	cout << "hello my friend. Today we..." << endl;
-	cin >> a;
 
-	if (a <= 1) {
 
-		cout << "cheloveki" << endl;
-	}
-	else
-	{
-		cout << "CHEBURASKA" << endl;
-	}
+
+
 	system("pause");
 	return 0;
 }
