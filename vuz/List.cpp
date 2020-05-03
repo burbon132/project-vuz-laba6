66812bd0b213833ff0517023a09ca13937082fbd
burbon132 <burbon132@users.noreply.github.com>
2020-05-03 13:21:16 +0300

    Добавление функционала

--- vuz/List.cpp
+++ vuz/List.cpp
@@ -2,10 +2,20 @@
 
 List::List()
 {
-
+	Size = 0;
+	head = nullptr;
 }
 
 List::~List()
 {
 
+}
+
+List::Student::Student(char* Name = "Unknown", char* LastName = "Unknown", char* TrainingDirection = "Unknown", int NumberGroup = 0, Student* pNext = nullptr)
+{
+	this->Name = Name;
+	this->LastName = LastName;
+	this->TrainingDirection = TrainingDirection;
+	this->NumberGroup = NumberGroup;
+	this->pNext = pNext;
 }
--- vuz/List.h
+++ vuz/List.h
@@ -3,10 +3,21 @@
 #define _LIST_H_
 class List
 {
+public:
 	List();
 	~List();
+private:
+	struct Student
+	{
+		Student* pNext;
+		char* Name = new char[10]; // Имя из 10 символов
+		char* LastName = new char[15]; // Фамилия из 15 символов
+		char* TrainingDirection = new char[5]; // Направление подготовки из 5 символов
+		int NumberGroup; // номер группы
+		Student(char* Name, char* LastName, char* TrainingDirection, int NumberGroup, Student* pNext);
+		//class DataBir; НУЖНО ДОБАВИТЬ ФУНКЦИОНАЛ СРАВНЕНИЯ ДАТЫ РОЖДЕНИЯ
+	};
+	int Size;
+	Student* head;
 };
 #endif // !_LIST_H_
-
-
-
